57b5a01078bcb7cf774cd5f2568cf31897c1eb4c
David Bayer <drbayer@eternalstench.com>
2023-05-01 17:26:42 -0700

    Auto-add changes to dotfiles on shell exit
    ## main...origin/main
     M profiles/common/01_env.sh

--- profiles/common/01_env.sh
+++ profiles/common/01_env.sh
@@ -12,7 +12,7 @@ export BASH_SILENCE_DEPRECATION_WARNING=1
 # shellcheck disable=SC2155
 export HOMEBREW_GITHUB_API_TOKEN="$(get_safe_value HOMEBREW_GITHUB_API_TOKEN)" 
 export PATH=/usr/local/opt/gnu-sed/libexec/gnubin:$PATH:$HOME/.bash.d/profiles/common/helpers:$HOME/.bash.d/profiles/active/helpers:/usr/local/opt/python/libexec/bin:$HOME/Library/Python/3.8/bin
-export PROMPT_COMMAND='history -a'  # add '; history -n' if you want to reread history file on each prompt
+export PROMPT_COMMAND='history -a; history -n'  # add '; history -n' if you want to reread history file on each prompt
 
 if [[ -e /opt/homebrew/bin/brew ]]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
