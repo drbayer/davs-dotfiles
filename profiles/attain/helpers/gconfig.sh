64bc86e51cbc6f40226c6437f4385edea51256d8
David Bayer <dbayer@attaindata.io>
2024-04-25 11:23:53 -0700

    Auto-add changes to dotfiles on shell exit
    Hostname: Davids-MacBook-Pro
    
    ## main...origin/main
     M profiles/attain/helpers/gconfig.sh

--- profiles/attain/helpers/gconfig.sh
+++ profiles/attain/helpers/gconfig.sh
@@ -2,10 +2,17 @@
 
 gconfig() {
 
+    # Print the active gcloud configuration
     current() {
+        echo "$1"
+        if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
+            echo "Print the currently active gcloud configuration name"
+            exit
+        fi
         gcloud config configurations list | awk '/True/ {print $1}'
     }
 
+    # Change to a different gcloud configuration
     switch() {
         local new_env=$1
         if [[ -z $new_env ]]; then
@@ -16,10 +23,12 @@ gconfig() {
         gcloud config configurations activate $new_env
     }
 
+    # List available gcloud configurations
     list() {
         gcloud config configurations list --format json | jq -r '.[] | .name'
     }
 
+    # Describe a gcloud configuration
     describe() {
         local this_env=$1
         if [[ -z this_env ]]; then
@@ -28,18 +37,28 @@ gconfig() {
         gcloud config configurations describe $this_env
     }
 
+    # Print this help message
     help() {
-        local f=$(basename "$0")
-        echo $f
+        local this_file=$(find "$HOME" -name gconfig.sh -type f -print -quit)
+        funcs=$(awk -F\( '/^[  ]+[a-z]+\(\)/ {gsub("[  ]+", ""); print $1}' "$this_file")
+        echo "Usage: gconfig COMMAND [FLAGS]"
+        echo
+        echo "Manage gcloud configurations."
+        echo "To get more help on a specific command use 'gconfig COMMAND -h'."
+        echo
+        echo "Available commands:"
+        for func in $funcs; do
+            egrep -B1 "^\s+${func}\(\)" "$this_file" | awk -v f=$func '/#/ {gsub("^[  ]+# ", ""); printf "    %-12s%s\n", f, $0}'
+        done
     }
 
 
     case $1 in
-        current)    current
+        current)    current $2
                     ;;
         switch)     switch $2
                     ;;
-        list)       list
+        list)       list $2
                     ;;
         describe)   describe $2
                     ;;
