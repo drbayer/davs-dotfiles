9a48710e69ca81a263c36bdd10abff691938af5d
David Bayer <dbayer@attaindata.io>
2024-10-30 11:51:40 -0700

    Auto-add changes to dotfiles on shell exit
    Hostname: ip-192-168-1-195
    
    ## main...origin/main
     M profiles/common/98_aliases.sh

--- profiles/common/98_aliases.sh
+++ profiles/common/98_aliases.sh
@@ -6,7 +6,6 @@ os=$(get_os)
 if [[ "$os" == "macos" ]]; then
     alias en='Cg=='
     alias flushdns='dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
-    alias grepv='grep -v '\''^$'\'' | grep -v '\''^#'\'''
     alias ls='ls -G'
     alias powershell='pwsh'
 fi
@@ -22,8 +21,6 @@ alias java8='export JAVA_HOME=`/usr/libexec/java_home -v1.8` && java -version'
 
 # Git
 alias repo='cd ${GIT_REPO_DIR}'
-alias gcommit='gitCommit'
-alias gmerge='gitMerge'
 
 # Python
 alias python='python3'
