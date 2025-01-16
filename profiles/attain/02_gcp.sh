2e1c61dd5b46a8a6116c5d1f40ff05dd34a8c01a
David Bayer <dbayer@attaindata.io>
2025-01-16 14:46:00 -0800

    Auto-add changes to dotfiles on shell exit
    Hostname: dbayer-mbp1
    
    ## main...origin/main
     M profiles/attain/02_gcp.sh
     M profiles/attain/homebrew/installs.txt
     M profiles/attain/logrotate/logrotate.status

--- profiles/attain/02_gcp.sh
+++ profiles/attain/02_gcp.sh
@@ -4,3 +4,5 @@ source "$(brew --prefix)/share/google-cloud-sdk/path.bash.inc"
 source "$(brew --prefix)/etc/bash_completion.d/google-cloud-sdk"
 
 source "$DOTFILES_BASEDIR/profiles/active/helpers/gconfig.sh"
+
+alias gcurl='curl -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json"'
