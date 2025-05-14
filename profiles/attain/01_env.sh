9e6f83f5051e1c809656edfb5f61521f0bfc22fb
David Bayer <dbayer@attaindata.io>
2025-05-13 17:34:13 -0700

    Auto-add changes to dotfiles on shell exit
    Hostname: dbayer-mbp1
    
    ## main...origin/main
     M profiles/attain/01_env.sh
     M profiles/attain/homebrew/installs.txt

--- profiles/attain/01_env.sh
+++ profiles/attain/01_env.sh
@@ -6,3 +6,7 @@ export kloverstats=kloverstats.c6zdrrll14uj.us-east-2.rds.amazonaws.com
 export PATH=$PATH:/opt/attain/bin:/opt/homebrew/opt/mysql-client/bin
 
 eval "$(thefuck --alias)"
+
+# gsutil requires python 3.12 or earlier, but system python is 3.13
+# /opt/homebrew/opt/python@3.12/bin/python3.12 -m venv gsutil_env
+alias gsutil='. ~/tmp/gsutil/gsutil_env/bin/activate; gsutil'
