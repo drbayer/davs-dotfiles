2cd0b089d7338ba31ec7667b0308f01d05b1df24
David Bayer <dbayer@attaindata.io>
2025-05-19 17:55:26 -0700

    Auto-add changes to dotfiles on shell exit
    Hostname: dbayer-mbp1
    
    ## main...origin/main
     M profiles/attain/01_env.sh
     M profiles/common/linters/yamllint

--- profiles/attain/01_env.sh
+++ profiles/attain/01_env.sh
@@ -1,6 +1,7 @@
 export GITHUB_SSH_COMMAND='ssh -i /Users/dbayer/.ssh/github-drbayer'
 
 # AWS RDS Hosts
+export devstats=devstats.c6zdrrll14uj.us-east-2.rds.amazonaws.com
 export initialstattest=initialstattest.c6zdrrll14uj.us-east-2.rds.amazonaws.com
 export kloverstats=kloverstats.c6zdrrll14uj.us-east-2.rds.amazonaws.com
 export PATH=$PATH:/opt/attain/bin:/opt/homebrew/opt/mysql-client/bin
