4ff36bbffa889bbb5472e2b5feb187f539f42701
David Bayer <dbayer@attaindata.io>
2025-08-26 10:20:31 -0700

    Auto-add changes to dotfiles on shell exit
    Hostname: dbayer-mbp1
    
    ## main...origin/main
     M profiles/attain/01_env.sh

--- profiles/attain/01_env.sh
+++ profiles/attain/01_env.sh
@@ -14,7 +14,8 @@ eval "$(thefuck --alias)"
 
 # gsutil requires python 3.12 or earlier, but system python is 3.13
 # /opt/homebrew/opt/python@3.12/bin/python3.12 -m venv gsutil_env
-alias gsutil='. ~/tmp/gsutil/gsutil_env/bin/activate; gsutil'
+# This has been fixed - saving alias definition for future reference
+# alias gsutil='. ~/tmp/gsutil/gsutil_env/bin/activate; gsutil'
 
 # we use many versions of terraform in sre-infrastructure and using a
 # newer version can cause grief. Use tfenv to compensate.
