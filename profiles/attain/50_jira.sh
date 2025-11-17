6e9a61561dd8717d685c1a6aac5f80f7e5a2e16b
David Bayer <dbayer@attaindata.io>
2025-11-17 15:01:56 -0800

    Auto-add changes to dotfiles on shell exit
    Hostname: dbayer-mbp1
    
    ## main...origin/main
    M  profiles/attain/50_jira.sh

--- profiles/attain/50_jira.sh
+++ profiles/attain/50_jira.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-scriptfile="${DOTFILES_BASEDIR}/profiles/active/helpers/jira_todoist.sh"
+# Disable jira_todoist - API updates required
+# scriptfile="${DOTFILES_BASEDIR}/profiles/active/helpers/jira_todoist.sh"
 
 if [[ -f "$scriptfile" ]]; then
     "$scriptfile"
