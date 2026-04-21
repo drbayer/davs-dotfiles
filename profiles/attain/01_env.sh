821b7123d19bc266c1f488b5192bd14cd8ecd4df
David Bayer <dbayer@joinklover.com>
2026-04-21 16:14:51 -0700

    Auto-add changes to dotfiles on shell exit
    Hostname: dbayer-mbp1
    
    ## main...origin/main [ahead 1]
     M profiles/attain/01_env.sh

--- profiles/attain/01_env.sh
+++ profiles/attain/01_env.sh
@@ -4,6 +4,7 @@ export GITHUB_SSH_COMMAND='ssh -i /Users/dbayer/.ssh/github-drbayer'
 export devstats=devstats.c6zdrrll14uj.us-east-2.rds.amazonaws.com
 export initialstattest=initialstattest.c6zdrrll14uj.us-east-2.rds.amazonaws.com
 export kloverstats=kloverstats.c6zdrrll14uj.us-east-2.rds.amazonaws.com
+export aurora_prod=klover-axis-production.cluster-c6zdrrll14uj.us-east-2.rds.amazonaws.com
 export PATH=$PATH:/opt/attain/bin:/opt/homebrew/opt/mysql-client/bin
 export JIRA_USER=$(get_safe_value JIRA_USER)
 export JIRA_USER_EMAIL=$(get_safe_value JIRA_USER)
