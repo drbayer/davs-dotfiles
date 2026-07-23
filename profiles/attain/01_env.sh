98feb2265a06b5884243c2ec0f0dc9d565b28a9e
David Bayer <dbayer@joinklover.com>
2026-07-23 11:04:06 -0700

    Auto-add changes to dotfiles on shell exit
    Hostname: dbayer-mbp1
    
    ## main...origin/main
     M profiles/attain/01_env.sh

--- profiles/attain/01_env.sh
+++ profiles/attain/01_env.sh
@@ -1,7 +1,7 @@
 export GITHUB_SSH_COMMAND='ssh -i /Users/dbayer/.ssh/github-drbayer'
 
 # AWS RDS Hosts
-export devstats=devstats.c6zdrrll14uj.us-east-2.rds.amazonaws.com
+export devstats=devstats.cv9fmu9rjt22.us-east-2.rds.amazonaws.com
 export initialstattest=initialstattest.c6zdrrll14uj.us-east-2.rds.amazonaws.com
 export kloverstats=kloverstats.c6zdrrll14uj.us-east-2.rds.amazonaws.com
 export kloverstats_rr=kloverstats-readreplica.c6zdrrll14uj.us-east-2.rds.amazonaws.com
